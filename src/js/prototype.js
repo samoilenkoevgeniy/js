34da44a182fcaba0d0296bcc1c1bf83fa2f1d57e
Evgeniy Samoylenko <Evgenii.Samoilenko@excelian.com>
2018-05-23 16:49:23 +0300

    little changes

--- src/js/prototype.js
+++ src/js/prototype.js
@@ -11,3 +11,21 @@ const User = {
 
 tomas = Object.create(User).constructor('tomas');
 tomas.sayHello();
+
+const WebDeveloper = Object.create(User);
+WebDeveloper.constructor = (name, skills) => {
+	User.constructor.apply(this, arguments);
+	this.skills = skills;
+
+	return this;
+};
+WebDeveloper.getSkills = () => {
+	console.log(this.skills);
+};
+
+const JackDeveloper = Object.create(WebDeveloper).constructor('Jack', [
+	'nodeJS', 'php'
+]);
+
+JackDeveloper.sayHello().getSkills();
+
