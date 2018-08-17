14cb34780e8789ae6fba1f80aef6dc423eb9e935
Evgeniy Samoylenko <Evgenii.Samoilenko@excelian.com>
2018-08-17 14:45:08 +0300

    little changes

--- src/js/test.js
+++ src/js/test.js
@@ -1,4 +1,18 @@
+
+setTimeout(() => {
+	console.log(123);
+}, 0);
+
+
 console.log(parseInt("1px"),
-parseInt("-1.2"),
-parseInt("0 минут"),
-parseInt("$1.2"));
+	parseInt("-1.2"),
+	parseInt("0 минут"),
+	parseInt("$1.2"));
+console.log(parseInt("1px"),
+	parseInt("-1.2"),
+	parseInt("0 минут"),
+	parseInt("$1.2"));
+console.log(parseInt("1px"),
+	parseInt("-1.2"),
+	parseInt("0 минут"),
+	parseInt("$1.2"));
