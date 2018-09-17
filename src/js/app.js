2b1e6cc6ece2316d750cbdf8204a3ec00f3d2fc1
evgeniysamoilenko <samoilenkoevgeniy@gmail.com>
2018-09-17 20:32:54 +0300

    changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,26 @@
-require('./stacked_call.js');
+// require('./stacked_call.js');
+// console.log('------');
+// require('./timeouts.js');
+// console.log('------');
+// require('./test.js');
+// console.log('------');
+// require('./inheritance.js');
+// console.log('------');
+// require('./closures.js');
+// console.log('------');
+// require('./context.js');
 console.log('------');
-require('./timeouts.js');
-console.log('------');
-require('./test.js');
-console.log('------');
-require('./inheritance.js');
-console.log('------');
-require('./closures.js');
-console.log('------');
-require('./context.js');
-console.log('------');
-require('./scope.js');
-console.log('------');
-require('./prototype.js');
-console.log('------');
-require('./links.js');
+// require('./scope.js');
+// console.log('------');
+// require('./prototype.js');
+// console.log('------');
+// require('./links.js');
+
+// Object.prototype.valueOf = function(){ return JSON.stringify( this ) }
+//
+// console.log( {a:{b:2}} === {a:{b:2}} );
+var obj = { name: 'amy', valueOf: () => 'test', toString: () => 'test', toPrimitive: () => true};
+
+var obj1 = { name: 'amy', valueOf: () => 'test', toString: () => 'test', toPrimitive: () => true};
+
+console.log( obj == obj1 );
